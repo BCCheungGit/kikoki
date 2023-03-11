70765e20d4d17c7d3019b8b48adde86dc08d355b
CheungBen <cheungbenjamin9@gmail.com>
2023-03-11 23:03:45 +0800

    added new stuff

--- src/App.js
+++ src/App.js
@@ -8,32 +8,20 @@ function App() {
   // React States
   const [errorMessages, setErrorMessages] = useState({});
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [enteredEmail, setEnteredEmail] = useState();
+  const [enteredPass, setEnteredPass] = useState();
 
-  // User Login info
-  const database = [
-    {
-      username: "user1",
-      password: "pass1"
-    },
-    {
-      username: "user2",
-      password: "pass2"
-    }
-  ];
-
-  const errors = {
-    uname: "invalid username",
-    pass: "invalid password"
-  };
 
   const handleSubmit = (event) => {
     //Prevent page reload
     event.preventDefault();
 
-    var { uname, pass } = document.forms[0];
-
+    var uname = enteredEmail;
+    var pass = enteredPass;
+    console.log(uname);
+    console.log(pass);
+    console.log('hi');
     // Find user login info
-    //const userData = database.find((user) => user.username === uname.value);
     Axios.post('http://localhost:5432/login', {
       email: uname,
       password: pass
@@ -47,39 +35,25 @@ function App() {
       }
     })
   }
-    // Compare user info
-    /*
-    if (userData) {
-      if (userData.password !== pass.value) {
-        // Invalid password
-        setErrorMessages({ name: "pass", message: errors.pass });
-      } else {
-        setIsSubmitted(true);
-      }
-    } else {
-      // Username not found
-      setErrorMessages({ name: "uname", message: errors.uname });
-    }
-  };
-  
-  // Generate JSX code for error message
-  const renderErrorMessage = (name) =>
-    name === errorMessages.name && (
-      <div className="error">{errorMessages.message}</div>
-    );
-*/
+    
   // JSX code for login form
   const renderForm = (
     <div className="form">
       <form onSubmit={handleSubmit}>
         <div className="input-container">
           <label>Username </label>
-          <input type="text" name="uname" required />
+          <input type="text"
+          onChange={(e) => {setEnteredEmail(e.target.value)}}   
+          required
+          />
           
         </div>
         <div className="input-container">
           <label>Password </label>
-          <input type="password" name="pass" required />
+          <input type="password" 
+          onChange={(e)=>{setEnteredPass(e.target.value)}}
+          required 
+          />
           
         </div>
         <div className="button-container">
